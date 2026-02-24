347dc44ec68c38847e6b6eca8f2826a685b5dfa1
RJA24 <ronjay.1204@gmail.com>
2026-02-24 15:54:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.markdown("""
     text-align: center;
     margin-bottom: 20px;
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3);
-    opacity 80%;
+    opacity: 0.7;
 }
 .custom-header h1 {
     color: white !important;
@@ -342,4 +342,5 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
+
 
